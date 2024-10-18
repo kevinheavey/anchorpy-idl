203a300f964fd58e752ba84d2c2c7545c0f25c3c
kevinheavey <heaveykevin2@gmail.com>
2024-10-18 23:45:22 +0400

    migrate rust code and get tests working with tutorial idls

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use idl::{
-    EnumFieldsNamed, EnumFieldsTuple, Idl, IdlAccount, IdlAccounts, IdlConst, IdlEnumVariant,
-    IdlErrorCode, IdlEvent, IdlEventField, IdlField, IdlInstruction, IdlPda, IdlSeedAccount,
-    IdlSeedArg, IdlSeedConst, IdlState, IdlTypeArray, IdlTypeDefined, IdlTypeDefinedWithTypeArgs,
-    IdlTypeDefinition, IdlTypeDefinitionTyAlias, IdlTypeDefinitionTyEnum,
-    IdlTypeDefinitionTyStruct, IdlTypeGeneric, IdlTypeGenericLenArray, IdlTypeOption,
-    IdlTypeSimple, IdlTypeVec,
+    Idl, IdlAccount, IdlConst, IdlDefinedFieldsNamed, IdlDefinedFieldsTuple, IdlEnumVariant, IdlErrorCode,
+    IdlEvent, IdlField, IdlInstruction, IdlInstructionAccount, IdlInstructionAccounts, IdlPda,
+    IdlSeedAccount, IdlSeedArg, IdlSeedConst, IdlTypeArray, IdlTypeDef, IdlTypeDefAlias,
+    IdlTypeDefEnum, IdlTypeDefStruct, IdlTypeDefined, IdlTypeGeneric,
+    IdlTypeOption, IdlTypeSimple, IdlTypeVec,
 };
 use pyo3::{
     prelude::*,
@@ -21,44 +20,46 @@ fn anchorpy_idl(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<IdlTypeOption>()?;
     m.add_class::<IdlTypeVec>()?;
     m.add_class::<IdlTypeArray>()?;
-    m.add_class::<IdlTypeGenericLenArray>()?;
-    m.add_class::<IdlTypeDefinedWithTypeArgs>()?;
     m.add_class::<IdlConst>()?;
     m.add_class::<IdlField>()?;
-    m.add_class::<IdlTypeDefinitionTyStruct>()?;
-    m.add_class::<EnumFieldsNamed>()?;
-    m.add_class::<EnumFieldsTuple>()?;
+    m.add_class::<IdlTypeDefStruct>()?;
+    m.add_class::<IdlDefinedFieldsNamed>()?;
+    m.add_class::<IdlDefinedFieldsTuple>()?;
     m.add_class::<IdlEnumVariant>()?;
-    m.add_class::<IdlTypeDefinitionTyEnum>()?;
-    m.add_class::<IdlTypeDefinitionTyAlias>()?;
-    m.add_class::<IdlTypeDefinition>()?;
-    m.add_class::<IdlAccounts>()?;
+    m.add_class::<IdlTypeDefEnum>()?;
+    m.add_class::<IdlTypeDefAlias>()?;
+    m.add_class::<IdlTypeDef>()?;
+    m.add_class::<IdlInstructionAccounts>()?;
     m.add_class::<IdlSeedConst>()?;
     m.add_class::<IdlSeedArg>()?;
     m.add_class::<IdlSeedAccount>()?;
     m.add_class::<IdlPda>()?;
-    m.add_class::<IdlAccount>()?;
+    m.add_class::<IdlInstructionAccount>()?;
     m.add_class::<IdlInstruction>()?;
-    m.add_class::<IdlState>()?;
     m.add_class::<IdlEvent>()?;
-    m.add_class::<IdlEventField>()?;
     m.add_class::<IdlErrorCode>()?;
     m.add_class::<IdlTypeGeneric>()?;
+    m.add_class::<IdlAccount>()?;
+    m.add_class::<IdlTypeDef>()?;
     m.add_class::<Idl>()?;
 
     let typing = py.import("typing")?;
     let union = typing.getattr("Union")?;
-    let idl_account_item_members = vec![IdlAccount::type_object(py), IdlAccounts::type_object(py)];
+    let idl_account_item_members = vec![
+        IdlInstructionAccount::type_object(py),
+        IdlInstructionAccounts::type_object(py),
+    ];
     m.add(
         "IdlAccountItem",
         union.get_item(PyTuple::new(py, idl_account_item_members))?,
     )?;
     let idl_type_definition_ty_members = vec![
-        IdlTypeDefinitionTyStruct::type_object(py),
-        IdlTypeDefinitionTyEnum::type_object(py),
+        IdlTypeDefAlias::type_object(py),
+        IdlTypeDefStruct::type_object(py),
+        IdlTypeDefEnum::type_object(py),
     ];
     m.add(
-        "IdlTypeDefinitionTy",
+        "IdlTypeDefTy",
         union.get_item(PyTuple::new(py, idl_type_definition_ty_members))?,
     )?;
     let idl_seed_members = vec![
@@ -75,8 +76,7 @@ fn anchorpy_idl(py: Python, m: &PyModule) -> PyResult<()> {
         IdlTypeOption::type_object(py),
         IdlTypeVec::type_object(py),
         IdlTypeArray::type_object(py),
-        IdlTypeDefinedWithTypeArgs::type_object(py),
-        IdlTypeGenericLenArray::type_object(py),
+        PyString::type_object(py)
     ];
     m.add(
         "IdlTypeCompound",
@@ -98,8 +98,8 @@ fn anchorpy_idl(py: Python, m: &PyModule) -> PyResult<()> {
         union.get_item(PyTuple::new(py, idl_defined_type_arg_members))?,
     )?;
     let enum_fields_members = vec![
-        EnumFieldsNamed::type_object(py),
-        EnumFieldsTuple::type_object(py),
+        IdlDefinedFieldsNamed::type_object(py),
+        IdlDefinedFieldsTuple::type_object(py),
     ];
     m.add(
         "EnumFields",
--- tests/test_idl.py
+++ tests/test_idl.py
@@ -3,10 +3,10 @@ from anchorpy_idl import (
     IdlField,
     IdlAccount,
     IdlTypeSimple,
-    IdlTypeDefinition,
-    IdlTypeDefinitionTyAlias,
+    IdlTypeDef,
+    IdlTypeDefAlias,
     IdlErrorCode,
-    IdlTypeDefinitionTyStruct,
+    IdlTypeDefStruct,
 )
 
 from pathlib import Path
@@ -23,74 +23,3 @@ def test_idls() -> None:
         idl = Idl.from_json(raw)
         idls.append(idl)
     assert idls
-
-
-def test_clientgen_example() -> None:
-    path = Path("tests/idls/clientgen_example_program.json")
-    raw = path.read_text()
-    idl = Idl.from_json(raw)
-    assert idl.version == "0.1.0"
-    assert idl.name == "example_program"
-    assert idl.docs is None
-    assert idl.constants == []
-    second_ix = idl.instructions[1]
-    assert second_ix.name == "initializeWithValues"
-    assert second_ix.docs is None
-    assert second_ix.returns is None
-    first_arg = second_ix.args[0]
-    assert first_arg == IdlField(name="boolField", docs=None, ty=IdlTypeSimple.Bool)
-    first_acc_for_ix = second_ix.accounts[0]
-    assert first_acc_for_ix == IdlAccount(
-        name="state",
-        is_mut=True,
-        is_signer=True,
-        is_optional=None,
-        docs=None,
-        pda=None,
-        relations=[],
-    )
-    fourth_ix = idl.instructions[3]
-    assert fourth_ix.name == "incrementStateWhenPresent"
-    first_acc_for_ix = fourth_ix.accounts[0]
-    assert first_acc_for_ix == IdlAccount(
-        name="firstState",
-        is_mut=True,
-        is_signer=False,
-        is_optional=True,
-        docs=None,
-        pda=None,
-        relations=[],
-    )
-    first_acc = idl.accounts[0]
-    assert first_acc.name == "State"
-    assert first_acc.docs is None
-    first_acc_ty = first_acc.ty
-    assert first_acc_ty
-    assert isinstance(first_acc_ty, IdlTypeDefinitionTyStruct)
-    assert first_acc_ty.fields[0] == IdlField(
-        name="boolField", docs=None, ty=IdlTypeSimple.Bool
-    )
-    first_type = idl.types[0]
-    assert first_type.name == "BarStruct"
-    assert first_type.docs is None
-    first_type_ty = first_type.ty
-    assert isinstance(first_type_ty, IdlTypeDefinitionTyStruct)
-    first_type_fields = first_type_ty.fields
-    assert first_type_fields is not None
-    first_field = first_type_fields[0]
-    assert first_field == IdlField(name="someField", docs=None, ty=IdlTypeSimple.Bool)
-    assert idl.events is None
-    assert idl.errors is not None
-    assert idl.errors[0] == IdlErrorCode(
-        code=6000, name="SomeError", msg="Example error."
-    )
-    assert idl.metadata is None
-
-
-def test_alias() -> None:
-    raw = (
-        '{"name":"TickArryBitmap","type":{"kind":"alias","value":{"array":["u64",8]}}}'
-    )
-    parsed = IdlTypeDefinition.from_json(raw)
-    ty = parsed.ty
-    assert isinstance(ty, IdlTypeDefinitionTyAlias)
